784de8feac4a219a2d676f220063953605ff7922
marcin <marcin.kasinski@gmail.com>
2018-04-10 12:43:00 +0200

    Add profile

--- docker/start.sh
+++ docker/start.sh
@@ -3,5 +3,5 @@
 #ls -l /
 #cat /etc/init.d/filebeat
 /etc/init.d/filebeat start
-#java -jar SpringBootWeb-0.0.1-SNAPSHOT.jar --spring.cloud.consul.discovery.ipAddress=$HOST --spring.cloud.consul.discovery.port=$PORT0 --spring.cloud.consul.host=$HOST
-java -jar SpringBootWeb-0.0.1-SNAPSHOT.jar
+#java -jar -Dspring.profiles.active=$SPRING_PROFILE SpringBootWeb-0.0.1-SNAPSHOT.jar --spring.cloud.consul.discovery.ipAddress=$HOST --spring.cloud.consul.discovery.port=$PORT0 --spring.cloud.consul.host=$HOST
+java -jar -Dspring.profiles.active=$SPRING_PROFILE SpringBootWeb-0.0.1-SNAPSHOT.jar
