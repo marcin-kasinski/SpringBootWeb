adc39a592e173b6973e1e429d55b5c77ba401738
marcin <marcin.kasinski@gmail.com>
2018-08-20 14:45:00 +0200

    .

--- src/main/java/mk/WebApp.java
+++ src/main/java/mk/WebApp.java
@@ -1,6 +1,7 @@
 package mk;
 
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,13 @@ public class WebApp {
 		System.out.println("Version 16.04.2018 (changed type to web)");
 		System.out.println("Environment variables");
 		
+//		UUID uniqueKey = UUID.randomUUID();
+//		String span=UUID.randomUUID().toString();
+		
+//		String spanid= (UUID.randomUUID().toString()).replace("-","").substring(0, 16);
+		
+//	    System.out.println ("uniqueKey:"+ span);		
+
 
 		Map<String, String> env = System.getenv();
 		for (String envName : env.keySet()) {
