9e0871c8b756b0c53aed37f464d968ebb9f6ad79
marcin <marcin.kasinski@gmail.com>
2018-09-21 08:54:07 +0200

    .

--- src/main/java/mk/metrics/SampleMetricBean.java
+++ src/main/java/mk/metrics/SampleMetricBean.java
@@ -13,14 +13,16 @@ import io.micrometer.core.instrument.Timer;
 public class SampleMetricBean {
 
 	private final Counter counter;
-	private final Timer timer;
+	private final Timer timer1;
+	private final Timer timer2;
 
 	public SampleMetricBean(MeterRegistry registry) {
 		
 		
 		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Registering");
 		this.counter = registry.counter("MKWEB_6_received.messages");
-		this.timer = registry.timer("MKWEB_exec_time");
+		this.timer1 = registry.timer("MKWEB_exec_time1");
+		this.timer2 = registry.timer("MKWEB_exec_time2");
 	}
 
 	public void handleMessage(String message) {
@@ -32,9 +34,10 @@ public class SampleMetricBean {
 
 	public void handleTimerNanoseconds(long duration) {
 		
-		System.out.println("handleTimer START / duration: nanoseconds : "+duration+" / seconds :"+duration/1000000000);
+		System.out.println("handleTimer START / duration: nanoseconds : "+duration+" / miliseconds :"+duration/1000000);
 		//this.timer.record(duration, TimeUnit.NANOSECONDS);
-		this.timer.record(Math.abs(duration/1000000000), TimeUnit.SECONDS);
+		this.timer1.record(Math.abs(duration/1000000), TimeUnit.MILLISECONDS);
+		this.timer2.record(Math.abs(duration/1000000000), TimeUnit.SECONDS);
 		System.out.println("handleTimer END");
 	}
 }
